00ef2e34cb0fc7446d9490dc0ad2e4bdf2f17c35
Bautista Simone <bsimone@itba.edu.ar>
2025-08-20 15:08:24 -0300

    Test pipeline 7

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Spring Boot desde 🚀";
+        return "Hello, Spring  desde 🚀";
     }
 }
